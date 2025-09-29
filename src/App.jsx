f2e736bcdd8e2864c52a1cd1f59d80f4a4d2dd1e
huytdn <saolailatoivay991@gmail.com>
2025-09-30 00:00:57 +0700

    Team + Contacts

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/Dashboard";
-// import Team from "./scenes/team";
+import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
 // import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -25,13 +25,13 @@ function App() {
         <CssBaseline />
         <div className="app">
           <Sidebar />
-          <main className="content">
+          <main className="content ">
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
+              <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
+              {/* <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
---
+++ src/scenes/contacts/index.jsx
@@ -0,0 +1,116 @@
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataContacts } from "../../data/mockData";
+import Header from "../../components/Header";
+import { useTheme } from "@mui/material";
+
+const Contacts = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      flex: 0.5,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "registrarId",
+      headerName: "Registrar ID",
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "city",
+      headerName: "City",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "zipCode",
+      headerName: "Zip Code",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+  ];
+
+  return (
+    <Box m="20px">
+      <Header
+        title="CONTACTS"
+        subtitle="List of Contacts for Future Reference"
+      />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+
+          "& .super-app-theme--header": {
+            backgroundColor: `${colors.blueAccent[700]} !important`,
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+          },
+        }}
+      >
+        <DataGrid rows={mockDataContacts} columns={columns} showToolbar />
+      </Box>
+    </Box>
+  );
+};
+
+export default Contacts;
