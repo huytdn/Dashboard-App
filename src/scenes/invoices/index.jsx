cb980fc9647a291f328a3ea996bcbd0560f0d476
huytdn <saolailatoivay991@gmail.com>
2025-09-30 00:30:15 +0700

    Incoives + Form

---
+++ src/scenes/invoices/index.jsx
@@ -0,0 +1,96 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataInvoices } from "../../data/mockData";
+import Header from "../../components/Header";
+
+const Invoices = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+
+      renderCell: (params) => (
+        <Typography color={colors.greenAccent[500]}>
+          ${params.row.cost}
+        </Typography>
+      ),
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+  ];
+
+  return (
+    <Box m="20px">
+      <Header title="INVOICES" subtitle="List of Invoice Balances" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaderCheckbox": {
+            backgroundColor: `${colors.blueAccent[700]} !important`,
+          },
+          "& .super-app-theme--header": {
+            backgroundColor: `${colors.blueAccent[700]} !important`,
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Invoices;
